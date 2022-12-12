b2a3a2a03f8549b19dd8b48263a4539241ca136c
Ahmed Shiref <103084673+Ahmedshiref72@users.noreply.github.com>
2022-12-12 22:07:16 +0200

    final

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,17 @@ Future<void> main() async {
   // بيتأكد ان كل حاجه هنا في الميثود خلصت و بعدين يتفح الابلكيشن
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    //   options:
+    // const FirebaseOptions(
+    //     apiKey: "AIzaSyAIDHvKqrJ4g4kz49y00xmMPaTE8mi1Y7Q",
+    //     authDomain: "teledoctor-94083.firebaseapp.com",
+    //     projectId: "teledoctor-94083",
+    //     storageBucket: "teledoctor-94083.appspot.com",
+    //     messagingSenderId: "134827706672",
+    //     appId: "1:134827706672:web:86101e76d071e9130920f5",
+    //     measurementId: "G-B22NTSV2D0" ),
+  );
   await CacheHelper.init();
   Widget widget;
   bool onBoarding =false;
@@ -27,7 +37,7 @@ Future<void> main() async {
     } else {
       widget = LoginScreen();
     }    }
-   else {
+  else {
     CacheHelper.saveData(key: 'onBoarding',value: false);
     onBoarding = await CacheHelper.getData(key: 'onBoarding');
     widget = OnBoardingScreen();
@@ -45,25 +55,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
 
-      providers: [
-        BlocProvider(create: (BuildContext context) =>AppCubit()..getUsers() ,
-        ),
-        BlocProvider(create: (BuildContext context) =>LoginCubit(),
-        )
+        providers: [
+          BlocProvider(create: (BuildContext context) =>AppCubit()..getUsers() ,
+          ),
+          BlocProvider(create: (BuildContext context) =>LoginCubit(),
+          )
 
-      ],
-      child:MaterialApp(
-            theme: ThemeData(
-            textTheme: GoogleFonts.lailaTextTheme(
+        ],
+        child:MaterialApp(
+          theme: ThemeData(
+            textTheme: GoogleFonts.amikoTextTheme(
               Theme.of(context).textTheme,
-        ),),
+            ),),
           debugShowCheckedModeBanner: false,
           home:startWidget,
 
 
-    )
+        )
     );
   }
 }
-
 
